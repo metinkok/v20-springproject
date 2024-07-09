a8ac166f2c421ad46066d7d04dc18d86426375e9
Metin <mmetinkok@gmail.com>
2024-07-09 21:16:58 +0300

    spring initializr
    spring initializr

---
+++ Spring/v20/src/main/java/com/example/v20/V20Application.java
@@ -0,0 +1,13 @@
+package com.example.v20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class V20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(V20Application.class, args);
+	}
+
+}
